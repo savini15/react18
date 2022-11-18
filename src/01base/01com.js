b593bcfec68051fad0c2f340e5dda90c1b0ed6c3
nyp <savini15@163.com>
2022-11-18 21:25:29 +0800

    init

---
+++ src/01base/01com.js
@@ -0,0 +1,9 @@
+export `class Test {
+    constructor(){
+  this.a = 1
+    }
+
+    hello(){
+        console.log('aaa')
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+const container = document.getElementById('root')
+ReactDOM.createRoot(container).render('hhhhsdsd')
