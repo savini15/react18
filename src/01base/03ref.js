fda6793e15d8a94f510e2fc494411eb6e17451ae
nyp <savini15@163.com>
2022-11-18 22:50:04 +0800

    ref

---
+++ src/01base/03ref.js
@@ -0,0 +1,25 @@
+import React ,{Component} from "react";
+export default class App extends Component{
+  show=()=>{
+    // console.log(this.refs.test.value)//字符串格式
+    console.log(this.test.current.value)
+  }
+
+  test=React.createRef()
+ render(){
+  
+  return (
+    <div>
+      {/* 字符串格式 在严格模式 React.StrictMode 存在潜在风险 重复*/}
+       {/* <input ref='test'/>
+
+       <button onClick={this.show}>value </button> */}
+
+
+<input ref={this.test}/>
+
+<button onClick={this.show}>value </button>
+    </div>
+  )
+ }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import React from 'react'/
 import ReactDOM from 'react-dom/client'
 // import Demo1 from './01base/01com'
-import Demo1 from './01base/02事件绑定'
+// import Demo1 from './01base/02事件绑定'
+import Demo1 from './01base/03ref'
 const container = document.getElementById('root')
 ReactDOM.createRoot(container).render(<Demo1/>)
